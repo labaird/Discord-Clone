12c9af79140eff34197118ff356099a1dcd0d99b
labaird <lindsay.a.baird@gmail.com>
2022-02-02 16:34:21 -0800

    Footer updated with relevant images and formatting

--- src/Footer/FooterHeader/FooterHeader.jsx
+++ src/Footer/FooterHeader/FooterHeader.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styles from './FooterHeader.module.css';
+import flag from '../../shared/flag/us-flag.png';
+import twitterLogo from '../../shared/social-media-icons/twitter.svg';
+import instagramLogo from '../../shared/social-media-icons/instagram.svg';
+import facebookLogo from '../../shared/social-media-icons/facebook.svg';
+import youtubeLogo from '../../shared/social-media-icons/youtube.svg';
 
 export default function FooterHeader() {
   return (
     <div className={styles.FooterHeader}>
       <h1>IMAGINE A PLACE</h1>
       <div className={styles.location}>
-        <img alt="country flag" />
+        <img className={styles.flag} alt="country flag" src={flag} />
         English, USA
       </div>
       <div className={styles.socialMedia}>
-        <img alt="Twitter" />
-        <img alt="Snapchat" />
-        <img alt="Facebook" />
-        <img alt="YouTube" />
+        <img alt="Twitter" src={twitterLogo} />
+        <img alt="Instagram" src={instagramLogo} />
+        <img alt="Facebook" src={facebookLogo} />
+        <img alt="YouTube" src={youtubeLogo} />
       </div>
     </div>
   );
